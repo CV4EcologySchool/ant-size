856dabf00778082806cd9965ee02c8c3e4fbbf39
nimirz <imirznat@umich.edu>
2022-08-02 16:54:53 -0700

    more viz

--- code/visualize.py
+++ code/visualize.py
@@ -1,18 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 import os
 import random
 from glob import glob
 import pandas as pd
 import cv2
 
-data = "s3://ant-size/data/"
 
 file_list = []
-for root, dirs, files in os.walk(data):
+for root, dirs, files in os.walk('../data'):
     for file in files:
         if file.endswith('.JPG'):
             file_list.append(file)
+
 
 date = []
 id1 = []
@@ -51,11 +52,24 @@ print('Weight range: '+str(df.mass.min()*1000)+'mg to '+str(df.mass.max()*1000)+
 print('Cameras used: '+str(len(df.camera.unique())))
 print('Dates filmed: '+str(len(df.date.unique())))
 
-images = []
 
-# select some random images
-for i in random.sample(range(0, len(file_list)), 10):
-  images.append(mpimg.imread(str(file_list[i])))
+plt.rcParams['figure.figsize'] = [12, 6]
+plt.rcParams['figure.dpi'] = 100
+plt.hist(df.mass*1000, bins = range(0, 30, 1))
+plt.xlabel('Mass (mg)')
+plt.ylabel('Number of images')
+plt.title('Images per category')
+#plt.grid(b=None)
+plt.tight_layout()
+plt.tick_params(axis='x', which='both', bottom=True, top=False)
+plt.tick_params(axis='y', which='both', right=False, left=True)
+plt.savefig('../fig1_hist.png')
+
+# images = []
+
+""" # select some random images
+for i in random.sample(range(0, len(fn_list)), 10):
+  images.append(mpimg.imread(fn_list[i]))
 
 plt.figure(figsize=(20,10))
 columns = 5
@@ -63,4 +77,4 @@ for i, image in enumerate(images):
     plt.subplot(len(images) / columns + 1, columns, i + 1)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-plt.savefig('ant_sample.png')
+plt.savefig('../ant_sample.png') """
